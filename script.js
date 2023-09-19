a16db2749474dba41f0ed657ffc1499f627f25ee
Abubakar50C <abubakara2005@gmail.com>
2023-09-18 23:35:57 -0400

    Made function to pull random object from an array

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-var love = ['?']
+const wordList=[
+    { word: "javascript",
+      hint: "Programming language"
+    
+    
+    },
+    { word: "air",
+    hint: "Inhale... Exhale"
+    
+    },
+    {
+        word: "milkyway",
+        hint: "galaxy"
+    }
+]
+
+function randomWord(){
+    let randObj = wordList[Math.floor(Math.random() * wordList.length)];
+    console.log(randObj)
+}
+randomWord()
