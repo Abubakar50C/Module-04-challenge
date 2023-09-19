fcd216e7198221fa2d16b93f16572e474bc63091
Abubakar50C <abubakara2005@gmail.com>
2023-09-18 23:48:43 -0400

    touching up code for getting random word

--- script.js
+++ script.js
@@ -14,8 +14,22 @@ const wordList=[
     }
 ]
 
+const inputs = document.querySelector('.inputs'),
+resetBtn = document.querySelector('.reset-btn'),
+hint = document.querySelector('.hint span');
+
 function randomWord(){
     let randObj = wordList[Math.floor(Math.random() * wordList.length)];
-    console.log(randObj)
+    let word = randObj.word;
+    console.log(word);
+
+    hint.innertext = randObj.hint
+
+    let html = ""
+    for (let i=0; i < word.length;i++){
+        html += '<input type="text" disabled>'
+
+    }
+    inputs.innerHTML = html;
 }
 randomWord()
